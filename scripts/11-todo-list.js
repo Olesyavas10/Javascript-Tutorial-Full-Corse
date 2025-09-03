9dd3b092fdbf0058c4713f2996b49e429dcb1671
OlesyaVas <olesyavas925@yandex.ru>
2025-09-03 15:16:52 +0700

    todo list done

--- scripts/11-todo-list.js
+++ scripts/11-todo-list.js
@@ -1,18 +1,45 @@
-const todoList=[];
+const todoList=[
+];
 function addTodo() {
     const inputElement=document.querySelector(".js-name-input");
-    const name=inputElement.value;
+
     
-    todoList.push(name);
+
+    const name=inputElement.value;
+    const dateInputElement=document.querySelector(".js-due-date-input");
+
+    const dueDate=dateInputElement.value;
+
+    todoList.push({
+        name,
+        dueDate
+    });
     inputElement.value="";
 
-    let todoListHTML='';
-for (let i=0; i<todoList.length; i++) {
-    const todo=todoList[i];
-    const html=`<p>${todo}</p>`;
-    todoListHTML+=html;
+  
 
+renderTodoList()
 }
-console.log(todoListHTML);
-document.querySelector(".js-todo-list").innerHTML=todoListHTML;
+
+function renderTodoList() {
+      let todoListHTML='';
+for (let i=0; i<todoList.length; i++) {
+    const todoObject=todoList[i];
+    const {name, dueDate}=todoObject;
+   
+    const html=`
+    <div>${name}</div>
+    <div>${dueDate} </div>
+     
+    <button onclick="
+    todoList.splice(${i}, 1);
+    renderTodoList();
+    " class="delete-todo-button">Delete</button>
+    
+    `;
+    todoListHTML+=html;
+   
 }
+ document.querySelector(".js-todo-list").innerHTML=todoListHTML }
+
+
